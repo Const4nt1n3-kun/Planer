1910eff04270cd77ce020172b04c7a3627dcaac3
Andrewsorokin13 <Andrewsorokin13@list.ru>
2025-04-22 22:08:48 +0300

    Initial Commit

---
+++ Planer/PlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanerApp.swift
+//  Planer
+//
+//  Created by Андрей Сорокин on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
